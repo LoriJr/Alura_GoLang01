7f979e08abeb425d921e1653fcf2569e4f5203df
LoriJr <jrsilva.lorivas@gmail.com>
2024-11-07 13:43:26 -0300

    exercicio1 atualizado

--- main.go
+++ main.go
@@ -3,21 +3,20 @@ package main
 import (
     "fmt"
 )
-
-func SemParametro() string {
-    return "Exemplo de função sem parâmetro!"
+//EXERCÍCIO
+type Conta struct {
+    saldo float64
 }
 
-func UmParametro(texto string) string {
-    return texto
-}
-
-func DoisParametros(texto string, numero int) (string, int) {
-    return texto, numero
+func (c *Conta) depositarDezReais() float64 {
+    return c.saldo + 10
 }
 
 func main() {
-    fmt.Println(SemParametro())
-    fmt.Println(UmParametro("Exemplo de função com um parâmetro"))
-    fmt.Println(DoisParametros("Passando dois parâmetros: essa string e o número", 10))
+    contaTeste := Conta{saldo: 10}
+
+    contaTeste.depositarDezReais()
+    contaTeste.depositarDezReais()
+
+    fmt.Println(contaTeste.depositarDezReais())
 }
