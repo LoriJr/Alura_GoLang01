fc55250ae476b768eada048a3a97a61fb1b2b8ce
LoriJr <jrsilva.lorivas@gmail.com>
2024-11-07 11:37:00 -0300

    comparando tipos modo não go

--- main.go
+++ main.go
@@ -10,18 +10,22 @@ type ContaCorrente struct {
 }
 
 func main() {
-	contaDoJunior := ContaCorrente{titular: "Junior", saldo: 100} //não precisa ter uma ordem, podemos remover alguns campos sem problemas
-	contaDaLais := ContaCorrente{"Lais", 456, 7891010, 200}       //dessa maneira, entende que são argumentos, então é necessário que todos os campoes estejam preenchidos, caso contrário use a primeira forma, delacrando a variável
-	fmt.Println(contaDoJunior, "\n", contaDaLais)
-
-	//outra forma de criar os ponteiros, através do new, a estrutura acima é mais a cara de Go
+	
 	var contaDaMaria *ContaCorrente
 	contaDaMaria = new(ContaCorrente)
 	contaDaMaria.titular = "Maria"
 	contaDaMaria.saldo = 500
 
-	fmt.Println(contaDaMaria) //esse imprime um & na frente da impressão pq ele meio que não reconhece o exato local da variável mas sabe quais atributos estão nela
-	fmt.Println(*contaDaMaria) //esse já imprime sem o & porque já conhece a qual variável se refere
-	//sem o * diz respeito ao endereço na da variável na memória
-	//com o * diz respeito ao conteúdo da variável
+	var contaDaMaria2 *ContaCorrente
+	contaDaMaria2 = new(ContaCorrente)
+	contaDaMaria2.titular = "Maria"
+	contaDaMaria2.saldo = 500
+
+	fmt.Println(contaDaMaria == contaDaMaria2) // nessa comparação mesmo que os dados sejam iguais, a comparação é do endereço na memória, que no caso é falso
+	fmt.Println(*contaDaMaria == *contaDaMaria2) // estamos comparando os dados das variáveis, que no caso são iguais e portanto, verdadeiro
+	fmt.Println(&contaDaMaria)  //endereço na memória 0xc00005e050 
+	fmt.Println(&contaDaMaria2) //endereço na memória 0xc00005e058
+	
+	
+	
 }
