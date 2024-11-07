dc1cfc86f36bb7faac78640d87e9df717eaaddaa
LoriJr <jrsilva.lorivas@gmail.com>
2024-11-07 13:09:49 -0300

    função sacar

--- main.go
+++ main.go
@@ -9,23 +9,22 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string { 
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo //podeSacar é um booleano, que aceita verdadeiro ou falso
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "saldo insuficiente"
+	}
+}
+
 func main() {
+	contaJunior := ContaCorrente{}
+	contaJunior.titular = "Junior"
+	contaJunior.saldo = 1000
 	
-	var contaDaMaria *ContaCorrente
-	contaDaMaria = new(ContaCorrente)
-	contaDaMaria.titular = "Maria"
-	contaDaMaria.saldo = 500
+	fmt.Println(contaJunior.Sacar(200))
+	fmt.Println(contaJunior.saldo)
 
-	var contaDaMaria2 *ContaCorrente
-	contaDaMaria2 = new(ContaCorrente)
-	contaDaMaria2.titular = "Maria"
-	contaDaMaria2.saldo = 500
-
-	fmt.Println(contaDaMaria == contaDaMaria2) // nessa comparação mesmo que os dados sejam iguais, a comparação é do endereço na memória, que no caso é falso
-	fmt.Println(*contaDaMaria == *contaDaMaria2) // estamos comparando os dados das variáveis, que no caso são iguais e portanto, verdadeiro
-	fmt.Println(&contaDaMaria)  //endereço na memória 0xc00005e050 
-	fmt.Println(&contaDaMaria2) //endereço na memória 0xc00005e058
-	
-	
-	
 }
