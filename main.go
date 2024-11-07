154043f8b50becc27191ec4aaf8bed4258208826
LoriJr <jrsilva.lorivas@gmail.com>
2024-11-07 18:39:30 -0300

    incluindo nova conta de maneira mais simples

--- main.go
+++ main.go
@@ -8,15 +8,7 @@ import (
 )
 
 func main() {
-	contaDoJunior := contas.ContaCorrente{Titular: clientes.Titular{
-		Nome:"LouJr",
-		CPF: "12345678912",
-		Profissao: "analista de sistemas"},
-		NumeroAgencia: 123,
-		NumeroConta: 123456,
-		Saldo:30}
-	fmt.Println(contaDoJunior)
-	//impressão fica assim {{LouJr 12345678912 analista de sistemas} 123 123456 30}
-	//são duas chaves, a primeira se refere aos dados da conta corrente, quanto a segunda se refere aos dados do titular, com nome cpf e profissão
-
+	clienteJunior := clientes.Titular{Nome:"Junior", CPF:"123.123.123-12",Profissao: "Dev",}
+	contaDoJunior := contas.ContaCorrente{Titular: clienteJunior,NumeroAgencia: 123,NumeroConta:123456, Saldo: 1000}
+    fmt.Println(contaDoJunior)
 }
