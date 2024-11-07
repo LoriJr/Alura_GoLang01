a1007ca479a644946536d81bd86de018c879dc17
LoriJr <jrsilva.lorivas@gmail.com>
2024-11-07 01:16:03 -0300

    criando uma struct

--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
+
 import "fmt"
 
-func main(){
-	var titular string = "Junior"
-	var numeroAgencia int = 589
-	var numeroConta  int = 123456
-	var saldo float64 = 125.50
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
 
-	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
-
-	var titular2 string = "Lais"
-	var numeroAgencia2 int = 555
-	var numeroConta2 int = 111333
-	var saldo2 float64 = 100
-	
-	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
+func main() {
+	fmt.Println(ContaCorrente{})
 }
