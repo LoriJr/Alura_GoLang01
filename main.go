599570d53b02ffa0bdf7183c54076e0f85ffa631
LoriJr <jrsilva.lorivas@gmail.com>
2024-11-07 19:44:57 -0300

    interface e novo tipo de conta

--- main.go
+++ main.go
@@ -6,9 +6,24 @@ import (
 	"github.com/LoriJr/Alura_GoLang01/contas"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 contadoJunior := contas.ContaPoupanca {}
 contadoJunior.Depositar(100)
-fmt.Print(contadoJunior.ObterSaldo())
+
+contaDoPai := contas.ContaCorrente {}
+contaDoPai.Depositar(500)
+PagarBoleto(&contadoJunior, 60)
+PagarBoleto(&contaDoPai, 400)
+
+fmt.Println(contadoJunior.ObterSaldo())
+fmt.Println(contaDoPai.ObterSaldo())
 
 }
